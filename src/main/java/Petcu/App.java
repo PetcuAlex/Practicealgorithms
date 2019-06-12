f4f04e07b413f0b108f7a16ec3c57cbb95f18990
Alex <adilex99@yahoo.com>
2019-06-12 11:28:09 -0700

    Firt algorithm

---
+++ src/main/java/Petcu/App.java
@@ -0,0 +1,23 @@
+package Petcu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public void displayAverage(double a, double b, double c){
+        System.out.println("Calculating average of: " +a + ", " + b + ", " + c);
+        double average = (a + b + c) / 3;
+        System.out.println("Average: " + average);
+    }
+
+
+
+
+    public static void main( String[] args )
+    {
+        App app = new App();
+        app.displayAverage(19,20.5,89);
+    }
+}
